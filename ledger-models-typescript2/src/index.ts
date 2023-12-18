627091ca3471454b14aca229ef02f8bb8f76578e
davidjdoherty <davidjdoherty@gmail.com>
2023-12-18 11:54:09 -0300

    Adding usage in index.ts to ensure its used

--- ledger-models-typescript2/src/index.ts
+++ ledger-models-typescript2/src/index.ts
@@ -1,2 +1,9 @@
 let message: string = 'Hello, World!';
 console.log(message);
+
+import { Person } from './gen/msg-readme_pb'
+
+// Use it directly
+const personInstance = new Person({ name: "Alice", years: 30 });
+
+console.log(personInstance);
