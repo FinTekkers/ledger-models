8a48242768ae314309501346b58fa769b966d4b9
davidjdoherty <davidjdoherty@gmail.com>
2024-04-25 09:50:00 -0400

    Moving uuid test into separate file

--- ledger-models-javascript/node/wrappers/models/utils/uuid.ts
+++ ledger-models-javascript/node/wrappers/models/utils/uuid.ts
@@ -52,16 +52,4 @@ class UUID {
   }
 }
 
-const test_uuid_bytes = [217, 98, 253, 240, 51, 225, 77, 157, 153, 155, 126, 195, 80, 240, 203, 119];
-const test_uuid = new UUID(test_uuid_bytes);
-assert(test_uuid.toString() == 'd962fdf0-33e1-4d9d-999b-7ec350f0cb77');
-const test_uuid_bytes_copy = UUID.fromString(test_uuid.toString());
-
-assert(
-  test_uuid_bytes.length === test_uuid_bytes_copy.length &&
-  test_uuid_bytes.every((value, index) => value === test_uuid_bytes_copy[index])
-);
-
-assert(UUID.random().toString().length == 36);
-
 export { UUID };
